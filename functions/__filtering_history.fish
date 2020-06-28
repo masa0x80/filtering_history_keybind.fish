8fc464a6a8ba25049d287ed0f0067ecdf4f21731
KIMURA Masayuki <masa0x80@gmail.com>
2020-06-28 20:22:37 +0900

    Fix indent

--- functions/__filtering_history.fish
+++ functions/__filtering_history.fish
@@ -1,8 +1,8 @@
 function __filtering_history
-  commandline | read -l buffer
-  history | angler "$ANGLER_QUERY_OPTION '$buffer'" | read -l command
-  if test -n "$command"
-    commandline $command
-  end
-  commandline -f repaint
+    commandline | read -l buffer
+    history | angler "$ANGLER_QUERY_OPTION '$buffer'" | read -l command
+    if test -n "$command"
+        commandline $command
+    end
+    commandline -f repaint
 end
